8da7877c8bc159bd0f49f293f7c3eb71b3ecaf8d
Oyefule <drelanorgent@gmail.com>
2023-09-21 23:27:43 +0100

    Find Largest Numbers in Sub-array - Golang

--- GoLang/Find _Largest _Numbers _in _Array/go.mod
+++ GoLang/Find _Largest _Numbers _in _Array/go.mod
@@ -1,4 +1,4 @@
-module example/hello
+module example/largestNum
 
 go 1.19
 
--- GoLang/Find _Largest _Numbers _in _Array/largestNum.go
+++ GoLang/Find _Largest _Numbers _in _Array/largestNum.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
-import "math"
+import (
+	"fmt"
+	// "math"
 
+	"rsc.io/quote"
+)
+
+//PROBLEM:
+/**
+ * Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+   Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+ */
+//SOLUTION
 func main()  {
-	fmt.Println("Hello")
-
 	fmt.Println(quote.Go())
+	arr := [][]int{{4, 5, 1, 3}, {13, 27, 18, 26}, {32, 35, 37, 39}, {1000, 1001, 857, 1}}
+	fmt.Println((largestNumInSubArray(arr)))
+}
+
+func findMax(arr []int) int {
+	if len(arr) == 0 {
+		return -1
+	}
+	max := arr[0]
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	return max
+}
+
+func largestNumInSubArray(arr [][]int) []int{
+	max := []int{}
+
+	for i := 0; i < len(arr); i++ {
+		max = append(max, findMax(arr[i]))
+	}
+	return max
 }

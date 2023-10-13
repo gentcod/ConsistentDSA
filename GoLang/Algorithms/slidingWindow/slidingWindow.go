23d949f01ddb30621e21edb68122aa08d6406e0e
Oyefule <drelanorgent@gmail.com>
2023-10-13 10:38:44 +0100

    Sliding Window- Go, code clean up

--- GoLang/Algorithms/slidingWindow/slidingWindow.go
+++ GoLang/Algorithms/slidingWindow/slidingWindow.go
@@ -31,18 +31,3 @@ func maxProfit(prices []int) int {
 
 	return maxP
 }
-// def max_profit(prices: list[int]) -> int:
-//    #Two pointers: left=buy, right=sell
-//    l, r = 0, 1
-//    maxP = 0
-
-//    while r < len(prices):
-//       if prices[l] < prices[r]:
-//          profit = prices[r] - prices[l]
-//          maxP = max(maxP, profit)
-
-//       else:
-//          l = r
-//       r += 1
-   
-//    return maxP

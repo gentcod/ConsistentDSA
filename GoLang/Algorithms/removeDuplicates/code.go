abab8d14016077c4a7655245bc1cecd14d1df215
Oyefule <drelanorgent@gmail.com>
2024-07-27 00:35:59 +0100

    Modified remove duplicates

--- GoLang/Algorithms/removeDuplicates/code.go
+++ GoLang/Algorithms/removeDuplicates/code.go
@@ -2,7 +2,7 @@ package removeDuplicates
 
 import (
 	"fmt"
-	"sort"
+	// "sort"
 )
 
 func Solve(arr []int) {
@@ -13,12 +13,13 @@ func removeDuplicates(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
-	k := 0
-	sort.Ints(nums)
-	for i := 1; i < len(nums); i++ {
-		if nums[i] != nums[k] {
-			k++
+	i := 0
+	// sort.Ints(nums)
+	for j := 1; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
 		}
 	}
-	return k
+	return i + 1
 }

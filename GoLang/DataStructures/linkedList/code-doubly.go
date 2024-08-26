15bf7efc769531547667403027fc3695be2c8578
Oyefule <drelanorgent@gmail.com>
2024-08-26 22:21:03 +0100

    Implemented Doubly LinkedlIst

--- GoLang/DataStructures/linkedList/code-doubly.go
+++ GoLang/DataStructures/linkedList/code-doubly.go
@@ -5,60 +5,63 @@ import (
 	"fmt"
 )
 
-type Node[T any] struct {
+type DNode[T any] struct {
 	Data T
-	Next *Node[T]
+	Prev *DNode[T]
+	Next *DNode[T]
 }
 
-type SinglyLinkedList[T any] struct {
+type DoublyLinkedList[T any] struct {
 	count int
-	head *Node[T]
-	tail *Node[T]
+	head *DNode[T]
+	tail *DNode[T]
 }
 
 // New helps to initialize the LinkedList with specific values
-func New[T any](values ...T) *SinglyLinkedList[T] {
-	list := SinglyLinkedList[T]{}
+func NewDoubly[T any](values ...T) *DoublyLinkedList[T] {
+	list := DoublyLinkedList[T]{}
 	
 	list.Add(values...)
 	return &list
 }
 
 // NewEmpty helps to initialize an empty LinkedList
-func NewEmpty() *SinglyLinkedList[any] {
-	list := SinglyLinkedList[any]{}
+func NewEmptyDoubly() *DoublyLinkedList[any] {
+	list := DoublyLinkedList[any]{}
 	
 	return &list
 }
 
 
 // IsEmpty checks if the LinkedList is empty
-func (s *SinglyLinkedList[T]) IsEmpty() bool {
+func (s *DoublyLinkedList[T]) IsEmpty() bool {
 	return s.head == nil
 }
 
 // Size returns the length or size of the LinkedList
-func (s *SinglyLinkedList[T]) Size() int {
+func (s *DoublyLinkedList[T]) Size() int {
 	return s.count
 }
 
 // Add adds a new node to the head, replacing the previous head of the LinkedList.
-func (s *SinglyLinkedList[T]) Add(values ...T) {
+func (s *DoublyLinkedList[T]) Add(values ...T) {
 	for _,value := range values {
-		// fmt.Println(value)
-		node := &Node[T]{Data: value}
+		node := &DNode[T]{Data: value}
 		node.Next = s.head
+		if s.count > 0 {
+			node.Next.Prev = node
+		}
 		s.head = node
-		s.count++
-
+		
 		if s.count == 1 {
 			s.tail = node.Next
 		}
+		s.count++
 	}
 }
 
 // InsertAt takes a value and an index and adds the node to the list based on the index.
-func (s *SinglyLinkedList[T]) InsertAt(value T, index int) error {
+func (s *DoublyLinkedList[T]) InsertAt(value T, index int) error {
 	if index < 0 || index > s.count {
 		return errors.New("index is out of bound")
 	}
@@ -68,7 +71,7 @@ func (s *SinglyLinkedList[T]) InsertAt(value T, index int) error {
 		return nil
 	}
 
-	element := &Node[T]{Data: value}
+	element := &DNode[T]{Data: value}
 
 	currentIndex := index
 	current := s.head
@@ -82,14 +85,16 @@ func (s *SinglyLinkedList[T]) InsertAt(value T, index int) error {
 	nextNode := current.Next
 
 	prev.Next = element
+	element.Prev = prev
 	element.Next = nextNode
+	nextNode.Prev = element
 	s.count++
 
 	return nil
 }
 
 // RemoveAt removes a node in a particular index
-func (s *SinglyLinkedList[T]) RemoveAt(index int) error {
+func (s *DoublyLinkedList[T]) RemoveAt(index int) error {
 
 	if index < 0 || index > s.count || index == s.count {
 		return errors.New("index is out of bound")
@@ -98,6 +103,7 @@ func (s *SinglyLinkedList[T]) RemoveAt(index int) error {
 	if index == 0 {
 		nextNode := s.head.Next
 		s.head = nil
+		nextNode.Prev = nil
 		s.head = nextNode
 		s.count--
 		return nil
@@ -115,7 +121,9 @@ func (s *SinglyLinkedList[T]) RemoveAt(index int) error {
 	new_next := data.Next
 
 	current.Next = new_next
+	new_next.Prev = current
 	data.Next = nil
+	data.Prev = nil
 	data = nil
 	s.count--
 
@@ -123,11 +131,11 @@ func (s *SinglyLinkedList[T]) RemoveAt(index int) error {
 }
 
 // Diplay iterates through the list and shows the data in the LinkedList
-func (s *SinglyLinkedList[T]) Diplay() {
+func (s *DoublyLinkedList[T]) Diplay() {
 	data := []any {}
 
 	current := s.head
-	prev := &Node[T]{}
+	prev := &DNode[T]{}
 
 	for i := s.count; i > 0; i-- {
 		if current == s.head {
@@ -135,7 +143,8 @@ func (s *SinglyLinkedList[T]) Diplay() {
 		} else if current == s.tail {
 			data = append(data, fmt.Sprintf("[Tail: %v]", current.Data))
 		} else {
-			data = append(data, fmt.Sprintf("[Data: %v]", current.Data))
+			// data = append(data, fmt.Sprintf("[Data: %v]", current.Data))
+			data = append(data, fmt.Sprintf("[Prev: %v]-[Data: %v]-[Next: %v]", current.Prev.Data, current.Data, current.Next.Data))
 		}
 
 		prev = current
---
+++ GoLang/DataStructures/linkedList/code-singly.go
@@ -0,0 +1,146 @@
+package linkedList
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Node[T any] struct {
+	Data T
+	Next *Node[T]
+}
+
+type SinglyLinkedList[T any] struct {
+	count int
+	head *Node[T]
+	tail *Node[T]
+}
+
+// New helps to initialize the LinkedList with specific values
+func NewSingly[T any](values ...T) *SinglyLinkedList[T] {
+	list := SinglyLinkedList[T]{}
+	
+	list.Add(values...)
+	return &list
+}
+
+// NewEmpty helps to initialize an empty LinkedList
+func NewEmptySingly() *SinglyLinkedList[any] {
+	list := SinglyLinkedList[any]{}
+	
+	return &list
+}
+
+
+// IsEmpty checks if the LinkedList is empty
+func (s *SinglyLinkedList[T]) IsEmpty() bool {
+	return s.head == nil
+}
+
+// Size returns the length or size of the LinkedList
+func (s *SinglyLinkedList[T]) Size() int {
+	return s.count
+}
+
+// Add adds a new node to the head, replacing the previous head of the LinkedList.
+func (s *SinglyLinkedList[T]) Add(values ...T) {
+	for _,value := range values {
+		// fmt.Println(value)
+		node := &Node[T]{Data: value}
+		node.Next = s.head
+		s.head = node
+		s.count++
+
+		if s.count == 1 {
+			s.tail = node.Next
+		}
+	}
+}
+
+// InsertAt takes a value and an index and adds the node to the list based on the index.
+func (s *SinglyLinkedList[T]) InsertAt(value T, index int) error {
+	if index < 0 || index > s.count {
+		return errors.New("index is out of bound")
+	}
+
+	if index == 0 {
+		s.Add(value)
+		return nil
+	}
+
+	element := &Node[T]{Data: value}
+
+	currentIndex := index
+	current := s.head
+
+	for currentIndex > 1 {
+		current = current.Next
+		currentIndex--
+	}
+
+	prev := current
+	nextNode := current.Next
+
+	prev.Next = element
+	element.Next = nextNode
+	s.count++
+
+	return nil
+}
+
+// RemoveAt removes a node in a particular index
+func (s *SinglyLinkedList[T]) RemoveAt(index int) error {
+
+	if index < 0 || index > s.count || index == s.count {
+		return errors.New("index is out of bound")
+	}
+
+	if index == 0 {
+		nextNode := s.head.Next
+		s.head = nil
+		s.head = nextNode
+		s.count--
+		return nil
+	}
+
+	currentIndex := index
+	current := s.head
+
+	for currentIndex > 1 {
+		current = current.Next
+		currentIndex--
+	}
+
+	data := current.Next
+	new_next := data.Next
+
+	current.Next = new_next
+	data.Next = nil
+	data = nil
+	s.count--
+
+	return nil
+}
+
+// Diplay iterates through the list and shows the data in the LinkedList
+func (s *SinglyLinkedList[T]) Diplay() {
+	data := []any {}
+
+	current := s.head
+	prev := &Node[T]{}
+
+	for i := s.count; i > 0; i-- {
+		if current == s.head {
+			data = append(data, fmt.Sprintf("[Head: %v]", current.Data))
+		} else if current == s.tail {
+			data = append(data, fmt.Sprintf("[Tail: %v]", current.Data))
+		} else {
+			data = append(data, fmt.Sprintf("[Data: %v]", current.Data))
+		}
+
+		prev = current
+		current = prev.Next
+	}
+
+	fmt.Println(data)
+}

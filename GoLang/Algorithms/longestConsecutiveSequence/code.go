5dc53e259004df356f9df1dd3296aae48034500a
Oyefule <drelanorgent@gmail.com>
2024-12-11 19:34:36 +0100

    Longest Consecutive Sequence - Golang

---
+++ GoLang/Algorithms/longestConsecutiveSequence/code.go
@@ -0,0 +1,55 @@
+package longestconsecutive
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Leetcode Problem: https://leetcode.com/problems/longest-consecutive-sequence/
+func Solve(nums []int) {
+	fmt.Println(longestConsecutive(nums))
+}
+
+func longestConsecutive(nums []int) (c int) {
+	if len(nums) == 1 {
+		return 1
+	}
+	sort.Ints(nums)
+	set := makeSet(nums)
+
+	count := 1
+	if len(set) == 1 {
+		return 1
+	}
+	for i, n := range set {
+		if i+1 >= len(set) {
+			return
+		}
+		if set[i+1]-n == 1 {
+			count++
+		} else {
+			count = 1
+		}
+		if count > c {
+			c = count
+		}
+	}
+	return
+}
+
+// makeSet takes a sorted list or arrays of integers and returns a list or array with only unique integers
+func makeSet(nums []int) (set []int) {
+	c := 0
+	for _, n := range nums {
+		if n == 0 {
+			if len(set) < 1 || set[len(set)-1] < 0 {
+				set = append(set, n)
+			}
+		}
+		if n != 0 && n != c {
+			set = append(set, n)
+			c = n
+		}
+	}
+	return
+}
